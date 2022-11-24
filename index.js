bcc4847cf9086d4c1d6f7c8afc8e7f291caf11fd
Thien Trieu <thien.trieu@telus.com>
2022-11-24 15:23:49 -0800

    fetchISSFlyOverTimes function added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -17,4 +17,16 @@ fetchCoordsByIP('66.183.9.217', (error, coordinates) => {
   }
 
   console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+
+const myCoords = { latitude: '49.0743308', longitude: '-122.5593218' };
+
+fetchISSFlyOverTimes(myCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });

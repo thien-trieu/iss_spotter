a27fbd9863bc6dac1323c7b2a8403d4aa341254e
Thien Trieu <thien.trieu@telus.com>
2022-11-24 14:21:12 -0800

    completed fetchMyIP function

--- iss.js
+++ iss.js
@@ -2,12 +2,20 @@ const request = require('request');
 
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
-  request(`https://api.ipify.org?format=json`, (error, response, body) => {
-
+  request(`https://ap.ipify.org?format=json`, (error, response, body) => {
+    
     if (error) {
       return callback(error, null);
     }
-    callback(null, body);
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const IP = JSON.parse(body);
+
+    callback(null, IP);
   });
 };
 
